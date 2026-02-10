9f6cf2662b50263d567a8f65a2ee815cc5b74209
johng <johng@garage.office.sports-pac.com>
2026-02-10 00:50:26 -0800

    Added new structs to manage headers for future use with encryption

--- radioApp/radio_app.h
+++ radioApp/radio_app.h
@@ -14,6 +14,25 @@
 #include "main.h"
 #include "sx1262.h"
 
+#define RADIOAPP_USE_BINARY_PACKET 1
+
+#pragma pack(push, 1)
+typedef struct {
+    uint8_t node_id;
+    uint8_t counter_le[4];
+} radio_hdr_t;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct {
+    radio_hdr_t hdr;
+    uint8_t payload_len;
+    uint8_t payload[64];
+} radio_msg_t;
+#pragma pack(pop)
+
+
+
 void RadioApp_Init(SX1262_Handle *sx);
 void RadioApp_Loop(void);
 void RadioApp_OnDio1Exti(uint16_t pin);
